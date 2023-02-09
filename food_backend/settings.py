48a0253237289b3c18d20f2363adb2b9a5cbd57b
mayureshgadekar <mayureshgadekar067@gmail.com>
2023-02-09 14:38:35 +0000

    Add existing project files to Git

--- food_backend/settings.py
+++ food_backend/settings.py
@@ -127,6 +127,25 @@ USE_L10N = True
 
 USE_TZ = True
 
+## X-Frame-Options
+#X_FRAME_OPTIONS = 'DENY'
+#X-Content-Type-Options
+#SECURE_CONTENT_TYPE_NOSNIFF = True
+## Strict-Transport-Security
+#SECURE_HSTS_SECONDS = 15768000
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#SECURE_HSTS_PRELOAD = True
+#SECURE_BROWSER_XSS_FILTER = True
+
+## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+#SECURE_SSL_REDIRECT = True 
+
+# for more security
+# CSRF_COOKIE_SECURE = True
+# CSRF_USE_SESSIONS = True
+# CSRF_COOKIE_HTTPONLY = True
+# SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SAMESITE = 'Strict'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
